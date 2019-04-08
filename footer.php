25951c6fd9a9ee1ccd28cc94befbe784b853a09e
audrey mertens <amertens@worldofdigits.com>
2019-04-08 17:31:18 +0200

    add contact form

--- footer.php
+++ footer.php
@@ -3,5 +3,22 @@
 	<script type="text/javascript" src="main.js"></script>
 	<?php wp_footer(); ?>
 
+	 <!--contact form-->
+	 <section class="contact_form_container">
+		 <div class="contact_form">
+			<h1 class="contact_title"> Une question?</h1>
+			<?php echo do_shortcode( '[contact-form-7 id="62" title="Formulaire de contact 1"]' ); ?>
+		 </div>	
+	</section>
+
+	
+
+	<div class="home_footer">
+		<img class="home_footer_logo" src="<?php bloginfo('template_url'); ?>/assets/images/fostplus.jpg" alt="recycle cans" />
+		<img class="home_footer_logo" src="<?php bloginfo('template_url'); ?>/assets/images/BxlProprete-Logo.svg" alt="recycle cans" />
+		<img class="home_footer_logo" src="<?php bloginfo('template_url'); ?>/assets/images/BxlProprete-LogoNL.svg" alt="recycle cans" />
+		<img class="home_footer_logo" src="<?php bloginfo('template_url'); ?>/assets/images/logo-Fostplus.svg" alt="recycle cans" />
+	</div>
+
 </body>
 </html>
