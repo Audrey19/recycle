afe7b98b1bedecfb25e9a6a2c6f42c913c07929a
audrey mertens <amertens@worldofdigits.com>
2019-04-09 17:16:39 +0200

    responsive

--- header.php
+++ header.php
@@ -22,7 +22,7 @@
   
     <?php wp_nav_menu(array('menu_id' => 'menu_container','theme_location' => 'primary' )); ?>
     <div class="logo-container">
-    <a href="http://localhost:3000/recycle/"><img class="logo" src="<?php bloginfo('template_url'); ?>/assets/images/logo-primederetour.png" alt="recycle cans" /></>
+    <a href="http://localhost:3000/recycle/"><img class="logo" src="<?php bloginfo('template_url'); ?>/assets/images/logo-primederetour.png" alt="recycle cans" /></a>
     </div>
     </header>
 
