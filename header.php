a71bf66097f8dce33fbfeeddb1b654a059e26e73
audrey mertens <amertens@worldofdigits.com>
2019-04-10 15:53:50 +0200

    creation page leiux de collecte

--- header.php
+++ header.php
@@ -8,6 +8,7 @@
     <link rel="shortcut icon" href="favicon.ico"/>
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); echo '/style.css?' . filemtime( get_stylesheet_directory() . '/style.css'); ?>">
     <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <script type="text/javascript" src="/scripts/emailpage.js"></script>
     <?php wp_head(); ?>
 
     <!--[if lt IE 9]>
@@ -22,11 +23,30 @@
   
     <?php wp_nav_menu(array('menu_id' => 'menu_container','theme_location' => 'primary' )); ?>
     <div class="logo-container">
+      <div class="logo-container-box">
     <a href="http://localhost:3000/recycle/"><img class="logo" src="<?php bloginfo('template_url'); ?>/assets/images/logo-primederetour.png" alt="recycle cans" /></a>
+      </div>
     </div>
+    
+ 
+      <button class="hamburger hamburger--slider" type="button">
+        <span class="hamburger-box">
+          <span class="hamburger-inner"></span>
+        </span>
+        <script>
+          var hamburger = document.querySelector(".hamburger");
+          var navigation = document.querySelector(".menu");
+          hamburger.addEventListener("click", function() {
+            hamburger.classList.toggle("is-active");
+            navigation.classList.toggle("is-active");
+          });
+        </script>
+      </button>
+
+
     </header>
 
-S
+
 
     
 
---
+++ scss/modele_collecte.php
@@ -0,0 +1,72 @@
+<?php
+/*
+Template Name: Lieux de collecte
+*/
+?>
+
+<?php get_header(); ?>
+
+
+<!-- google map -->
+<section class="map_container"></section>
+
+
+<!-- location points -->
+<section class="locations_container">
+    <div class="locations_container_box1">
+        <div class="locations_container_box_title">
+            <img class="location_logo" src=""/>
+            <h1>ucl</h1>
+        </div>
+        <div class="locations_container_box_address">
+           <h2> ampus AlmaRue Jardin Martin V, 561200 Bruxelles</h2>
+        </div>
+        <div class="locations_container_box_contact">
+            <p> Contact en cas de problème avec la machine : espaces-exterieurs-gtpw@uclouvain.be</p>
+            <p> 02.764.44.30 (téléphone du secrétariat des services techniques, ouvert en permanence pendant les jours ouvrables et heures de service).</p>
+        </div> 
+    </div>
+    <div class="locations_container_box2">
+        <div class="locations_container_box_title">
+            <img class="location_logo" src=""/>
+            <h1>ulb</h1>
+        </div>
+        <div class="locations_container_box_address">
+         <h2>ampus AlmaRue Jardin Martin V, 561200 Bruxelles</h2>
+        </div>
+        <div class="locations_container_box_contact">
+            <p> Contact en cas de problème avec la machine : espaces-exterieurs-gtpw@uclouvain.be</p>
+            <p> 02.764.44.30 (téléphone du secrétariat des services techniques, ouvert en permanence pendant les jours ouvrables et heures de service).</p>
+        </div> 
+    </div>
+    <div class="locations_container_box3">
+        <div class="locations_container_box_title">
+            <img class="location_logo" src=""/>
+            <h1>ville de bruxelles</h1>
+        </div>
+        <div class="locations_container_box_address">
+            <h2>ampus AlmaRue Jardin Martin V, 561200 Bruxelles</h2>
+            <h2>ampus AlmaRue Jardin Martin V, 561200 Bruxelles</h2>
+        </div>
+        <div class="locations_container_box_contact">
+            <p> Contact en cas de problème avec la machine : espaces-exterieurs-gtpw@uclouvain.be</p>
+            <p> 02.764.44.30 (téléphone du secrétariat des services techniques, ouvert en permanence pendant les jours ouvrables et heures de service).</p>
+        </div> 
+    </div>
+
+</section>
+
+
+<!-- contact person -->
+<section class="contact_container">
+    <div class="contact-container-box">
+        <h1>Personnes de contact pour le projet prime de retour</h1>
+        <p>Hélène Vossen (Bruxelles Environnement) : 32 2 775 76 69 • hvossen@leefmilieu.brussels</p>
+        <p>Aurore Van Bogaert (Fost Plus) : 02 775 97 23 • 0475 55 24 05 • aurore.vanbogaert@fostplus.be</p>
+    </div>
+
+</section>
+
+
+
+<?php get_footer(); ?>
